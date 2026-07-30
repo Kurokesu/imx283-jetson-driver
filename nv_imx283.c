4d8de560dea2c5d53f10b8101a8a4925c638cae8
DaniusKalv <danius@kurokesu.com>
2026-07-30 20:56:44 +0000

    Replace strncpy with strscpy

--- nv_imx283.c
+++ nv_imx283.c
@@ -736,7 +736,7 @@ static int imx283_probe(struct i2c_client *client,
 
 	priv->i2c_client = tc_dev->client = client;
 	tc_dev->dev = dev;
-	strncpy(tc_dev->name, "imx283", sizeof(tc_dev->name));
+	strscpy(tc_dev->name, "imx283", sizeof(tc_dev->name));
 	tc_dev->dev_regmap_config = &sensor_regmap_config;
 	tc_dev->sensor_ops = &imx283_common_ops;
 	tc_dev->v4l2sd_internal_ops = &imx283_subdev_internal_ops;
